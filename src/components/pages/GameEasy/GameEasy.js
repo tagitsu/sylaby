9e09bbf0535280ed919b2648f882bac459d8ee9d
Julia Diaków <tagitsu@gmail.com>
2023-01-12 15:16:08 +0100

    Add styles to EasyGame component

--- src/components/pages/GameEasy/GameEasy.js
+++ src/components/pages/GameEasy/GameEasy.js
@@ -1,38 +1,60 @@
 import { useSelector } from "react-redux";
 import { useState } from 'react';
+import styles from './GameEasy.module.scss';
+import clsx from "clsx";
 
 const  GameEasy = () => {
 
   const { syllables } = useSelector(state => state.syllables);
 
-  const [ randomSyllable, setRandomSyllable ] = useState('');
+  const [ randomFirstSyllable, setRandomFirstSyllable ] = useState('');
+  const [ randomLastSyllables, setRandomLastSyllables ] = useState([]);
   const [ word, setWord ] = useState('');
 
-  const getRandomSyllable = () => {
+
+  const getRandomSyllable = (e) => {
+    e.preventDefault();
     const randomSyllableId = Math.floor(Math.random() * syllables.length);
     const [ randomSyllableObj ] = syllables.filter( syllable => syllable.id == randomSyllableId);
-    console.log('random syl obj', randomSyllableObj);
     if(randomSyllableObj.words.length > 0) {
-      setRandomSyllable(randomSyllableObj.name);
+      setRandomFirstSyllable(randomSyllableObj.name);
       const wordIndex = Math.floor(Math.random() * randomSyllableObj.words.length);
-      console.log('index wyrazu', wordIndex);
       const word = randomSyllableObj.words[wordIndex];
       setWord(word);
-      console.log('wylosowane słowo', word);
       const lastSyllable = word.substring(2);
-      console.log('druga sylaba', lastSyllable);
+      randomLastSyllables.push(lastSyllable);
+      for (let i = 0; i < 4; i++) {
+        const randomSyllableId = Math.floor(Math.random() * syllables.length);
+        const [ randomSyllableObj ] = syllables.filter( syllable => syllable.id == randomSyllableId);
+        randomLastSyllables.push(randomSyllableObj.name);
+      }
+      randomLastSyllables.sort();
     } else { getRandomSyllable() }
+    // [TODO] blokuj przycisk
   }
-  
-
-  
+ 
   return(
-    <div>
-      <button onClick={() => getRandomSyllable()}>Losuj sylabę</button>
-      <div>wylosowana sylaba {randomSyllable}</div>
-      <div>słowo {word}</div>
+    <div className={styles.easy} >
+      <button className={styles.easy__btn} onClick={(e) => getRandomSyllable(e)}>Losuj sylabę</button>
+      <div className={styles.easy__board}>
+        <div>
+          <div className={clsx(styles.easy__syllable, styles.first)}>{randomFirstSyllable}</div>
+        </div>
+        <div>
+          {randomLastSyllables.map( solution => 
+            <div className={styles.easy__word} key={solution}></div>
+          )}
+        </div>
+        <div>
+          {randomLastSyllables.map( lastSyllable => 
+            <div className={clsx(styles.easy__syllable, styles.last)} key={lastSyllable}>{lastSyllable}</div>
+          )}
+        </div>
+      </div>
+      <button className={styles.easy__btn} onClick={() => console.log('to jest moja odpowiedź')}>OK</button>
+
     </div>
   );
 };
 
-export default  GameEasy;
+export default GameEasy;
