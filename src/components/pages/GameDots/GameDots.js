eab298317295aa77b9a247cd290ee37c2cb9ccf8
Julia Diaków <tagitsu@gmail.com>
2023-04-18 13:30:11 +0200

    Add opacity to dots in dots game

--- src/components/pages/GameDots/GameDots.js
+++ src/components/pages/GameDots/GameDots.js
@@ -24,7 +24,7 @@ const GameDots = () => {
   }
 
   const [ dots, setDots ] = useState([]);
-  const [ answer, setAnswer ] = useState(0);
+  const [ answer, setAnswer ] = useState('');
   
   let dotsBoard;
 
