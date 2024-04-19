a2ab1745f1011301c73a209506aef9262095b414
Julia Diaków <tagitsu@gmail.com>
2024-04-19 14:37:01 +0200

    Change logo

--- src/components/views/Logo/Logo.js
+++ src/components/views/Logo/Logo.js
@@ -4,11 +4,13 @@ const Logo = () => {
   return(
       <ul className={styles.logo}>
         <li className={styles.logo__bubble}>
-          <span className={styles.logo__syllable}>sy</span>
+          <div className={styles.logo__syllable}>sy</div>
         </li>
+        <li className={styles.logo__mini}></li>
         <li className={styles.logo__bubble}>
           <span className={styles.logo__syllable}>la</span>
         </li>
+        <li className={styles.logo__mini}></li>
         <li className={styles.logo__bubble}>
           <span className={styles.logo__syllable}>ba</span>
         </li>
